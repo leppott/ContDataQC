4140152d069079a283fca2dbce9c610e4b8fdc5d
leppott <Erik.Leppo@tetratech.com>
2017-09-19 21:27:36 -0400

    v2.0.1.9039
    Vignette added.  Also added Excel visualization file to \extdata.

--- NEWS.md
+++ NEWS.md
@@ -2,7 +2,7 @@ NEWS-ContDataQC
 ================
 
 <!-- NEWS.md is generated from NEWS.Rmd. Please edit that file -->
-    #> Last Update: 2017-09-12 15:43:23
+    #> Last Update: 2017-09-19 20:59:18
 
 Version history.
 
@@ -16,6 +16,15 @@ Planned Updates
 -   Use futile.logger to better log output for user. Issue \#29. 20170606.
 
 -   Add option for Report output for PDF or DOCX or HTML. Defaults to DOCSX. Will need to change documentation for report functions. 20170823.
+
+v2.0.1.9039
+===========
+
+2017-09-19
+
+-   Vignette added. 20170919.
+
+-   Added daily mean visualization Excel file to . 20170919.
 
 v2.0.1.9038
 ===========
---
+++ R/Export.ST.R
@@ -0,0 +1,175 @@
+#' Export data for StreamThermal package
+#'
+#' Creates a date frame (and file export) from Continuous Data in the format used by the StreamThermal package.
+#'
+#' Required fields are StationID, Date, dailyMax, dailyMin, and dailyMean
+#' The fields are named "siteID", "Date", "MaxT", "MinT", and "MeanT".
+#'
+#' The StreamThermal package is available on GitHub.  It can be installed withe the devtools package:
+#'
+#' library(devtools)
+#'
+#' install_github("tsangyp/StreamThermal")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Erik.Leppo@tetratech.com (EWL)
+# 20170911
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @param fun.myFile Filename (no directory) of data file.  Must be CSV file.  Includes Date/Time and Discharge fields.
+#' @param fun.myDate.Format Format of benchmark date.  This should be the same format of the date in the data file.  Default is \%Y-\%m-\%d (e.g., 2017-12-31).
+#' @param fun.myDir.import Directory for import data.  Default is current working directory.
+#' @param fun.myDir.export Directory for export data.  Default is current working directory.
+#' @param fun.myDateRange.Start Start date for IHA analysis.  File will be filtered on this date.  Default is NA (no filtering).
+#' @param fun.myDateRange.End End date for IHA analysis.  File will be filtered on this date.  Default is NA (no filtering).
+#' @param fun.myCol.DateTime Column name in myFile with Date/Time.  Assumes format of \%Y-\%m-\%d \%H:\%M:\%S.  Default = "Date.Time".
+#' @param fun.myCol.Parameter Column name in myFile for measurements.  Default = "Discharge.ft3.s".
+#' @return Returns a data frame with daily mean values by date (in the specified range).  Also, a csv file is saved to the specified directory with the prefix "IHA" and the date range before the file extension.
+#' @keywords continuous data, daily mean, IHA
+#' @examples
+#' # 1.  Get Gage Data
+#' #
+#' # 1.A. Use ContDataQC and Save (~1min for download)
+#' myData.Operation    <- "GetGageData" #Selection.Operation[1]
+#' myData.SiteID       <- "01187300" # Hubbard River near West Hartland, CT
+#' myData.Type         <- "Gage"
+#' myData.DateRange.Start  <- "2015-01-01"
+#' myData.DateRange.End    <- "2016-12-31"
+#' myDir.import <- getwd()
+#' myDir.export <- getwd()
+#' ContDataQC(myData.Operation, myData.SiteID, myData.Type
+#'            , myData.DateRange.Start, myData.DateRange.End
+#'            , myDir.import, myDir.export)
+#' #
+#' # 1.B. Use saved data
+#' myFile <- "01187300_Gage_20150101_20161231.csv"
+#' myCol.DateTime="Date.Time"
+#' myCol.Discharge <- "Water.Temp.C"
+#' #
+#' # 2. Prep Data
+#' myData.ST <- Export.IHA(myFile
+#'                        , fun.myCol.DateTime = myCol.DateTime
+#'                        , fun.myCol.Parameter = myCol.Discharge
+#'                        )
+#'
+#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' # 3. Run StreamThermal
+#' # Example using returned DF with StreamThermal
+#' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' #~~~~~
+#' # Library (install if needed)
+#' # devtools::install_github("tsangyp/StreamThermal")
+#' # Library (load)
+#' require(StreamThermal)
+#' #~~~~~
+#' # StreamThermal
+#'
+#' ST.freq <- T_frequency(sitedata)
+#' ST.mag  <- T_magnitude(sitedata)
+#' ST.roc  <- T_rateofchange(sitedata)
+#' ST.tim  <- T_timing(sitedata)
+#' ST.var  <- T_variability(sitedata)
+#'
+#'
+#' myYr <- "calendar" # "water" or "calendar"
+#' # IHA Metrics
+#' ## IHA parameters group 1; Magnitude of monthly water conditions
+#' Analysis.Group.1 <- group1(myData.IHA, year=myYr)
+#' ## IHA parameters group 2: Magnitude of monthly water condition and include 12 parameters
+#' Analysis.Group.2 <- group2(myData.IHA, year=myYr)
+## IHA parameters group 3; Timing of annual extreme water conditions
+#' Analysis.Group.3 <- group3(myData.IHA, year=myYr)
+#' ## IHA parameters group 4; Frequency and duration of high and low pulses
+#' # defaults to 25th and 75th percentiles
+#' Analysis.Group.4 <- group4(myData.IHA, year=myYr)
+#' ## IHA parameters group 5; Rate and frequency of water condition changes
+#' Analysis.Group.5 <- group5(myData.IHA, year=myYr)
+#' #~~~~~
+#' # Save Results to Excel (each group on its own worksheet)
+#' Group.Desc <- c("Magnitude of monthly water conditions"
+#'                 ,"Magnitude of monthly water condition and include 12 parameters"
+#'                 ,"Timing of annual extreme water conditions"
+#'                 ,"Frequency and duration of high and low pulses"
+#'                 ,"Rate and frequency of water condition changes")
+#' df.Groups <- as.data.frame(cbind(paste0("Group",1:5),Group.Desc))
+#' #
+#' myDate <- format(Sys.Date(),"%Y%m%d")
+#' myTime <- format(Sys.time(),"%H%M%S")
+#' # Notes section (add min/max dates)
+#' Notes.Names <- c("Dataset (SiteID)","IHA.Year","Analysis.Date (YYYYMMDD)","Analysis.Time (HHMMSS)","Analysis.User")
+#' Notes.Data <- c(SiteID, myYr, myDate, myTime, Notes.User)
+#' df.Notes <- as.data.frame(cbind(Notes.Names,Notes.Data))
+#' Notes.Summary <- summary(myData.IHA)
+#' # Open/Create file
+#' myFile.XLSX <- paste("IHA", SiteID, myYr, myDate, myTime, "xlsx", sep=".")
+#' wb <- loadWorkbook(myFile.XLSX, create = TRUE) # load workbook, create if not existing
+#' # create sheets
+#' createSheet(wb, name = "NOTES")
+#' createSheet(wb, name = "Group1")
+#' createSheet(wb, name = "Group2")
+#' createSheet(wb, name = "Group3")
+#' createSheet(wb, name = "Group4")
+#' createSheet(wb, name = "Group5")
+#' # write to worksheet
+#' writeWorksheet(wb, df.Notes, sheet = "NOTES", startRow=1)
+#' writeWorksheet(wb, Notes.Summary, sheet = "NOTES", startRow=10)
+#' writeWorksheet(wb, df.Groups, sheet="NOTES", startRow=25)
+#' writeWorksheet(wb, Analysis.Group.1, sheet = "Group1")
+#' writeWorksheet(wb, Analysis.Group.2, sheet = "Group2")
+#' writeWorksheet(wb, Analysis.Group.3, sheet = "Group3")
+#' writeWorksheet(wb, Analysis.Group.4, sheet = "Group4")
+#' writeWorksheet(wb, Analysis.Group.5, sheet = "Group5")
+#' # save workbook
+#' saveWorkbook(wb, myFile.XLSX)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # QC Function
+# fun.myFile <- myFile
+# fun.myDir.import=getwd()
+# fun.myDir.export=getwd()
+# fun.myDateRange.Start <- NA
+# fun.myDateRange.End <- NA
+# fun.myCol.DateTime <- myCol.DateTime
+# fun.myCol.Discharge <- myCol.Discharge
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @export
+Export.IHA <- function(fun.myFile
+                      ,fun.myDir.import=getwd()
+                      ,fun.myDir.export=getwd()
+                      ,fun.myDateRange.Start=NA
+                      ,fun.myDateRange.End=NA
+                      ,fun.myCol.DateTime="Date.Time"
+                      ,fun.myCol.Parameter="Discharge.ft3.s"
+                      )
+{##FUNCTION.Export.IHA.START
+  # import file
+  myDF <- read.csv(myFile, stringsAsFactors = FALSE)
+  myCols <- c(fun.myCol.DateTime, fun.myCol.Parameter)
+  # add date
+  myDF <- myDF[,myCols]
+  myDF[,"Date"] <- as.Date(myDF[,fun.myCol.DateTime],format="%Y-%m-%d")
+  # filter for date range supplied by user (if NA, default, do nothing)
+  if(!is.na(fun.myDateRange.Start)){
+    date.start <- as.Date(fun.myDateRange.Start, format="%Y-%m-%d")
+    myDF <- myDF[myDF[,"Date"]>=date.start,]
+  } else {
+    date.start <- "NA"
+  }
+  if(!is.na(fun.myDateRange.End)){
+    date.end <- as.Date(fun.myDateRange.End, format="%Y-%m-%d")
+    myDF <- myDF[myDF[,"Date"]<=date.end,]
+  } else {
+    date.end <- "NA"
+  }
+  # generate daily values
+  data.daily <- aggregate(myDF[,2] ~ myDF[,3], FUN=mean)
+  names(data.daily) <- c("Date", fun.myCol.Parameter)
+  #head(data.daily)
+  # Create zoo object for use with IHA library
+  myData <- zoo::zoo(data.daily[,2],order.by=data.daily[,1])
+  # Save
+  myFile.base <- substr(fun.myFile,1,nchar(fun.myFile)-4)
+  myFile.ext <- substr(fun.myFile,nchar(myFile.base)+1,nchar(fun.myFile))
+  myFile.dates <- paste0("_",date.start,"_",date.end)
+  myFile.IHA <- paste0("IHA_",myFile.base,myFile.dates,myFile.ext)
+  write.csv(myData,myFile.IHA)
+  # Return DF to user
+  return(myData)
+}##FUNCTION.Export.IHA.END
