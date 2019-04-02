613a2244c55c4b8d38132e1191e7bccd1e958663
leppott <Erik.Leppo@tetratech.com>
2019-04-02 13:23:46 -0400

    v2.0.5.9020
    Update version number.

---
+++ config.ExcludeFailsFalse.R
@@ -0,0 +1,21 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Exclude Trigger ####
+# Trigger for Stats to exclude (TRUE) or include (FALSE) where flag = "fail"
+ContData.env$myStats.Fails.Exclude <- FALSE  #FALSE #TRUE
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
