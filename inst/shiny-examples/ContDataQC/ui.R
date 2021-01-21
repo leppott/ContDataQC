81c1bdaa95e0e4a91dc1df882c084263d4884de7
Erik Leppo <Erik.Leppo@tetratech.com>
2021-01-21 15:17:13 -0500

    v2.0.5.9072
    * refactor: Report_QC.rmd, add Water Level vs. Water Temp plots, Issue #110
    * style: Report_QC.rmd, trim lines to 80 characters
    * refactor: Copy Report_QC.rmd to shiny

--- inst/shiny-examples/ContDataQC/ui.R
+++ inst/shiny-examples/ContDataQC/ui.R
@@ -6,7 +6,7 @@
 
 shinyUI(
   # VERSION ----
-  navbarPage("Continuous data QC, summary, and statistics - v2.0.5.9068",
+  navbarPage("Continuous data QC, summary, and statistics - v2.0.5.9072",
              theme= shinytheme("spacelab"), #also liked "cerulean" at https://rstudio.github.io/shinythemes/
             # tabPan, Site Intro ----
              tabPanel("Site introduction",
