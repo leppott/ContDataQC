5235a03bb4f8332393210d2fe9eac2c42ba33686
Blocktt <68349347+Blocktt@users.noreply.github.com>
2021-06-17 10:56:57 -0400

    v2.0.5.9097
    Updated tab 6 information

--- inst/shiny-examples/ContDataQC/external/tab_6Console.R
+++ inst/shiny-examples/ContDataQC/external/tab_6Console.R
@@ -2,26 +2,11 @@
 
 function() {
 
-  tabPanel("6. Console",
-             ## _Adv Feat, Console ----
-
-               h3("R console output", align = "Center"),
-               p("This panel shows messages output by the QC, aggregating, summarizing, and USGS data retrieval processes.
-                                 If there are any errors when the tool runs, please copy
-                                 the messages and send them and your input files to the contacts listed on the 'Site introduction' tab."),
-               tableOutput("logText"),
-               tableOutput("logTextUSGS"),
-               tags$b(textOutput("logTextMessage"))
-
-               ## For debugging only: lists all files on the server
-               # ,br()
-               # ,br()
-               # ,br()
-               # ,br()
-               # ,tableOutput("serverTable")
-
-
-
+  tabPanel("6. Console"
+           ,includeHTML("App_6Console.html")
+           ,tableOutput("logText")
+           ,tableOutput("logTextUSGS")
+           ,tags$b(textOutput("logTextMessage"))
 
   ) ## tabPanel ~ Adv Feat ~ END
 
