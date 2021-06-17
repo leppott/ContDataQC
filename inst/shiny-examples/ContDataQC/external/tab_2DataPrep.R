443e8e873f46e8cd070c1d715648b07a58974124
Blocktt <68349347+Blocktt@users.noreply.github.com>
2021-06-17 10:37:14 -0400

    v2.0.5.9096
    Updated 2d HOBOware reformat

--- inst/shiny-examples/ContDataQC/external/tab_2DataPrep.R
+++ inst/shiny-examples/ContDataQC/external/tab_2DataPrep.R
@@ -11,30 +11,22 @@ function() {
                                     , img(src = "Fig_Organizing_v1_20210616.png"
                                           , height = 600, width = 1000)
 
-                         )## tabPanel ~ CURRENT ~ END
+                         )## tabPanel ~ END
 
                          , tabPanel("Naming files"
                                     , includeHTML("App_2bDataPrep.html")
                                     , img(src = "Fig_NamingFiles_v1_20210617.png"
                                           , height = 600, width = 1000)
-                         )## tabPanel ~ Streams ~ END
+                         )## tabPanel~ END
 
                          , tabPanel("Formatting files"
                                     , includeHTML("App_2cDataPrep.html")
-                         )## tabPanel ~ Streams ~ END
+                         )## tabPanel ~ END
 
                          , tabPanel("HOBOware Reformat"
-                                    , tags$b("Description")
-                                    , p("Format HoboWare output for use with 'ContDataQC' package. Works on individual csv files. Imports, modifies, and saves with the same filename for download.")
-                                    , p("One file or multiple files can be uploaded for processing.")
-                                    , tags$b("Details")
-                                    , p("Imports a HoboWare output (with minimal tweaks), reformats it using defaults from the ContDataQC config file , and exports a CSV to another folder for use with ContDataQC.")
-                                    , p("Older versions of HOBOware  only output ambiguous dates with 2 digits.
-                                              Newer versions use 4 digits.  There are two delimiters (/ and -) and three formats (MDY, DMY, and YMD) resulting in six possible formats.")
-                                    , p("The user can specify the date format below.  'DMY' is the default as this is what most people use and is the default on Windows computers.  NULL will not modify the input dates.")
-                                    , p("It is assumed the user has a single Date Time field rather than two fields (Date and Time).")
-                                    , p("A zip file with all CSV files ready for use with the ContDataQC QC function will be generated and can be downloaded.")
-                                    , p("'Run' and 'Download' buttons do not appear until the previous step is complete ('Upload' and 'Run', respectively).")
+                                    , includeHTML("App_2dDataPrep.html")
+                                    , img(src = "Fig_HOBOreformat_v1_20210617.png"
+                                          , height = 600, width = 1000)
                                     , hr()
                                     , tags$div(title="Select one HOBOware csv files to upload here"
                                                , fileInput("selectedFiles_HOBO"
@@ -60,7 +52,7 @@ function() {
                                               uiOutput('ui.downloadData_HOBO')
                                     )# tags$div ~ END
 
-                         )## tabPanel ~ Lakes ~ END
+                         )## tabPanel  ~ END
 
              )## tabsetPanel ~ END
 
