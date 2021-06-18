e245c9dd0110e49684106fa75d497cca47f78e63
Blocktt <68349347+Blocktt@users.noreply.github.com>
2021-06-18 09:11:46 -0400

    v2.0.5.9098
    Updated 3a section

--- inst/shiny-examples/ContDataQC/external/tab_3QCThresh.R
+++ inst/shiny-examples/ContDataQC/external/tab_3QCThresh.R
@@ -3,44 +3,40 @@
 function() {
 
   tabPanel("3. QC Thresholds"
-           , sidebarPanel(
-             h3("QC thresholds", align = "Center")
-             , p("You can upload custom QC thresholds here.
-                                 Please use",
-                 a("this ", target="_blank", href="Config_Template.zip"),
-                 "configuration document as a template.")
-             , p("Once you have made your changes to the configuration file, upload them below.
-                                 You will have the option to return to the default configuration after you upload your custom file.
-                                 Note that if the newenv() line (33) does not stay commented out it will not work.")
-             , br()
-             #Tool tip code from https://stackoverflow.com/questions/16449252/tooltip-on-shiny-r
-             , tags$div(title="Select R configuration file to upload here",
-
-                        #Only allows R files to be imported
-                        fileInput("configFile"
-                                  , label = "Choose configuration file"
-                                  , multiple = FALSE
-                                  , accept = ".R")
-             ) # tag$div ~ END
-
-             , br()
-             , br()
-
-             #Only shows the "Default configuration" button after a user-selected file has been used
-             ,tags$div(title = "Click to use default configuration data",
-                       uiOutput('ui.defaultConfig'))
-
-
-           )## sidebarPanel ~ END
         , mainPanel(
           tabsetPanel(type = "tabs"
 
             , tabPanel("Default"
-              , h4("Default - fill info", align = "Center")
+              , h4("Default", align = "Center")
             )## tabPanel ~ CURRENT ~ END
 
             , tabPanel("Customize"
-                       , h4("Customize - fill info", align = "Center")
+                       , h4("Custom", align = "Center")
+                       , p("You can upload custom QC thresholds here.
+                                 Please use",
+                           a("this ", target="_blank", href="Config_Template.zip"),
+                           "configuration document as a template.")
+                       , p("Once you have made your changes to the configuration file, upload them below.
+                                 You will have the option to return to the default configuration after you upload your custom file.
+                                 Note that if the newenv() line (33) does not stay commented out it will not work.")
+                       , br()
+                       #Tool tip code from https://stackoverflow.com/questions/16449252/tooltip-on-shiny-r
+                       , tags$div(title="Select R configuration file to upload here",
+
+                                  #Only allows R files to be imported
+                                  fileInput("configFile"
+                                            , label = "Choose configuration file"
+                                            , multiple = FALSE
+                                            , accept = ".R")
+                       ) # tag$div ~ END
+
+                       , br()
+                       , br()
+
+                       #Only shows the "Default configuration" button after a user-selected file has been used
+                       ,tags$div(title = "Click to use default configuration data",
+                                 uiOutput('ui.defaultConfig'))
+
             )## tabPanel ~ Streams ~ END
 
           )## tabsetPanel ~ END
